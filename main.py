c63983c6ff5b8461598d8c8ca4e2a9718ac07ae7
djanux03 <80212550+djanux03@users.noreply.github.com>
2021-04-20 23:39:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,34 +2,50 @@ from urllib import request
 from bs4 import BeautifulSoup
 import cloudscraper
 import discord
-
+from discord.ext import commands
 TOKEN = "ODIxODU2NDI0NzYwOTY3MTY5.YFJzcQ.w8VpmqAjun8vBrgolTPDWrBtPKM"
-site = "https://cod.tracker.gg/warzone/profile/battlenet/"
-username = "BabaBlanco%232902/overview"
 bot = discord.Client()
+bot = commands.Bot(command_prefix="!")
 
-url = site + username
-scraper = cloudscraper.create_scraper()
-cod = scraper.get(url).text 
+site = "https://cod.tracker.gg/warzone/profile/battlenet/"
 
-soup = BeautifulSoup(cod, 'html.parser')
-title = soup.find('title')
+
+def insertAfter(usersInput, context, newText):
+ 
+  i = usersInput.find(context)
+  return usersInput[:i + len(context)] + newText + usersInput[i + len(context):]
 
 @bot.event
 async def on_message(message):
-    if message.content == "cod":
+
+    if message.content.startswith("!"):
+        input = message.content
+        input = input.replace('!', '')
+        c = "#"
+        
+        #print([pos for pos, char in enumerate(input) if char == c])
+        
+        print(input)
+        #context = scraper(input)
+        #print(context)      
+   
+        newinput = insertAfter(input, c, "23")
+        userinput = newinput.replace('#', '%')
+        
+        print(userinput)
+        
+
+        fullurl = site + userinput
+        scraper = cloudscraper.create_scraper()
+        cod = scraper.get(fullurl).text 
+
+        soup = BeautifulSoup(cod, 'html.parser')
+       
+     
         for div in soup.find_all('div', class_='numbers'):
             fact_title = div.find('span', class_='name').string
             #print(fact_title.string)
             num = div.find('span', class_='value').string
             await message.channel.send("```" +fact_title + ": " + num.string + "```")
-      
-
-def get_stats():
-            
-    
-    return num
-#, num
-
 
 bot.run(TOKEN)
