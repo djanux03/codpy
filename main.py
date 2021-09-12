451f87f1c284f45e11317ac38adc85b9737d251f
djanux03 <80212550+djanux03@users.noreply.github.com>
2021-09-12 11:10:16 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from urllib import request
+from bs4 import BeautifulSoup
+import cloudscraper
 import discord
 from discord.ext import commands
-TOKEN = "ODIxODU2NDI0NzYwOTY3MTY5.YFJzcQ.k-KGO5qC7pzbLePZnWEMEbzen9M"
+TOKEN = "ODIxODU2NDI0NzYwOTY3MTY5.YFJzcQ.RMimsezFMK1Yojn3ElxQovJRbLU"
 bot = discord.Client()
 bot = commands.Bot(command_prefix="!")
 
@@ -8,32 +11,21 @@ site = "https://cod.tracker.gg/warzone/profile/battlenet/"
 class codpy(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
+        self.data = ''
     def insertAfter(self, usersInput, context, newText):
         i = usersInput.find(context)
         return usersInput[:i + len(context)] + newText + usersInput[i + len(context):]
 
     @commands.Cog.listener()
     async def on_message(self, message):
-
         if message.content.startswith("!"):
             input = message.content
             input = input.replace('!', '')
             c = "#"
 
-                
-        #print([pos for pos, char in enumerate(input) if char == c])
-        
-        print(input)
-        #context = scraper(input)
-        #print(context)      
-   
         newinput = self.insertAfter(input, c, "23")
         userinput = newinput.replace('#', '%')
-        
         print(userinput)
-        
-
         fullurl = site + userinput
         scraper = cloudscraper.create_scraper()
         cod = scraper.get(fullurl).text 
@@ -45,7 +37,10 @@ class codpy(commands.Cog):
             fact_title = div.find('span', class_='name').string
             #print(fact_title.string)
             num = div.find('span', class_='value').string
-            await message.channel.send("```" +fact_title + ": " + num.string + "```")
+            self.data += fact_title + ": " + num + "\n"
+        await message.channel.send("```" + self.data + "\n```")
+        self.data = ''
+
 
 bot.add_cog(codpy(bot))
 bot.run(TOKEN)
