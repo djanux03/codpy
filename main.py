ba0a4915697458b50ecef5fef2f338d14389d768
djanux03 <80212550+djanux03@users.noreply.github.com>
2021-09-14 11:38:41 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from urllib import request
 from bs4 import BeautifulSoup
 import cloudscraper
+from PIL import Image, ImageDraw, ImageFont
 import discord
 from discord.ext import commands
-TOKEN = "ODIxODU2NDI0NzYwOTY3MTY5.YFJzcQ.RMimsezFMK1Yojn3ElxQovJRbLU"
+
+TOKEN = "ODIxODU2NDI0NzYwOTY3MTY5.YFJzcQ._iZ-otj9yBqglbB2O86tRdMhF1E"
 bot = discord.Client()
 bot = commands.Bot(command_prefix="!")
 
@@ -16,6 +18,13 @@ class codpy(commands.Cog):
         i = usersInput.find(context)
         return usersInput[:i + len(context)] + newText + usersInput[i + len(context):]
 
+    def add_margin(pil_img, top, right, bottom, left, color):
+        width, height = pil_img.size
+        new_width = width + right + left
+        new_height = height + top + bottom
+        result = Image.new(pil_img.mode, (new_width, new_height), color)
+        result.paste(pil_img, (left, top))
+        return result
     @commands.Cog.listener()
     async def on_message(self, message):
         if message.content.startswith("!"):
@@ -38,9 +47,20 @@ class codpy(commands.Cog):
             #print(fact_title.string)
             num = div.find('span', class_='value').string
             self.data += fact_title + ": " + num + "\n"
-        await message.channel.send("```" + self.data + "\n```")
-        self.data = ''
+        
+        #await message.channel.send("```" + self.data + "\n```")
+        
+        with Image.open('background.png') as im:
+            d = ImageDraw.Draw(im)
+            font = ImageFont.truetype('LucidaGrande.ttf', 94)
+            d.text((10, 10), self.data, font=font, fill=(0, 0, 0,))
+            im.save('warzone.png')
+        
+            self.data = ''
 
+        with open('warzone.png', 'rb') as f:
+            picture = discord.File(f)
+            await message.channel.send(file=picture)
 
 bot.add_cog(codpy(bot))
 bot.run(TOKEN)
